4375937c901236b806a40b687d8432409c8cbb2d
freshnewkicks <tylerdukedev@gmail.com>
2022-05-12 20:15:20 -0400

    added tailwind/flowbite

---
+++ components/modals/MailingList.jsx
@@ -0,0 +1,12 @@
+
+
+export default function MailingList() {
+
+    return (
+        <div>
+            <div className="position absolute">
+                <div className="w-[680px] h-[500px]"></div>
+            </div>
+        </div>
+    )
+}
