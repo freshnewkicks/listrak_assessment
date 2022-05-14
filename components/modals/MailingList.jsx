d097fca808a99a23f17071a65d90dd3dceeb4f65
freshnewkicks <tylerdukedev@gmail.com>
2022-05-14 09:30:45 -0400

    added 'X' icon for closing modal in MailingList.jsx

--- components/modals/MailingList.jsx
+++ components/modals/MailingList.jsx
@@ -13,6 +13,13 @@ export default function MailingList() {
             {currentSlide === 'registration' ? (
                     <div className="position absolute top-1/2 bottom-1/2 flex flex-col justify-center align-center items-center shadow">
                         <div className="sm:max-w-[680px] sm:max-h-[500px] max-w-[350px] max-h-[546px] bg-white shadow flex-col border border-black flex ">
+                            <div className="flex justify-end align-center items-center  px-2">
+                                <span className="w-[32px] h-[32px] flex justify-center items-center text-center text-[#e9e9e9]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </span>
+                            </div>
                             <div className="flex flex-col sm:flex-row w-full justify-center align-center items-center">
                                 <div className="flex flex-col w-full text-center sm:text-[65px] text-[45px] text-[#464646] tracking-widest sm:ml-24 mt-8 sm:mt-12 sm:mb-24">Get Your Style On</div>
                                 <div className="flex text-[65px] text-[#FD8C94] justify-center align-center items-center text-right w-full">&</div>
